de6d8a4f919ac3670f7c572ce864ab633f155e14
Adam James McCarthy <a.mccarthy.uk@gmail.com>
2017-08-05 20:56:33 +0200

    feat: Added a matrix test for linear subclass

--- MiniFlow/nn.py
+++ MiniFlow/nn.py
@@ -11,6 +11,7 @@
     examples.
 """
 
+import numpy as np
 from miniflow import (
                       Input,
                       Add,
@@ -69,5 +70,28 @@ def linear():
 
     print(output) # should be 12.7 with this example
 
+
+def linear_matrix():
+    X, W, b = Input(), Input(), Input()
+
+    f = Linear(X, W, b)
+
+    X_ = np.array([[-1., -2.], [-1, -2]])
+    W_ = np.array([[2., -3], [2., -3]])
+    b_ = np.array([-3., -5])
+
+    feed_dict = {X: X_, W: W_, b: b_}
+
+    graph = topological_sort(feed_dict)
+    output = forward_pass(f, graph)
+
+    """
+    Output should be:
+    [[-9., 4.],
+    [-9., 4.]]
+    """
+    print(output)
+
+
 if __name__ == "__main__":
-    addition_2()
+    linear()
