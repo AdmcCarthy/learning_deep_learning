7bc2e0cf003c4525cdbefc230f4d14dc7953cecc
Adam James McCarthy <a.mccarthy.uk@gmail.com>
2017-08-05 20:02:02 +0200

    feat: Remove test code example from MiniFlow
    MiniFlow will be used as a module with the test code stored outside this file

--- MiniFlow/miniflow.py
+++ MiniFlow/miniflow.py
@@ -66,14 +66,19 @@ class Add(Node):
         """
 
 
-# Define 2 `Input` nodes.
-x, y = Input(), Input()
+def forward_pass(output_node, sorted_nodes):
+    """
+    Performs a forward pass through a list of sorted nodes.
 
-# Define an `Add` node, the two above `Input` nodes being the input.
-add = Add(x, y)
+    Arguments:
 
-# The value of `x` and `y` will be set to 10 and 20 respectively.
-feed_dict = {x: 10, y: 20}
+        `output_node`: The output node of the graph (no outgoing edges).
+        `sorted_nodes`: a topologically sorted list of nodes.
+    
+    Returns the output node's value
+    """
 
-# Sort the nodes with topological sort.
-sorted_nodes = topological_sort(feed_dict=feed_dict)
+    for n in sorted_nodes:
+        n.forward()
+
+    return output_node.value
