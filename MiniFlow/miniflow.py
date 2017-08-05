1635bc702a746d0b516f73cf72b285d2b0ec931a
Adam James McCarthy <a.mccarthy.uk@gmail.com>
2017-08-05 21:49:34 +0200

    feat: Sigmoid activation function

--- MiniFlow/miniflow.py
+++ MiniFlow/miniflow.py
@@ -4,13 +4,13 @@
     MiniFlow
     ~~~~~~~~
 
-    A neural network implementation
-    done only using Numpy.
+    A neural network implementation.
 
     Based on material in Udacity
     Deep Learning Nanodegree.
 """
 
+import numpy as np
 
 class Node(object):
     def __init__(self, inbound_nodes=[]):
@@ -34,12 +34,13 @@ class Node(object):
         """
         raise NotImplemented
 
+
 class Input(Node):
     def __init__(self):
         # An Input node has no inbound nodes,
         # so no need to pass anything to the Node instantiator.
         Node.__init__(self)
-    
+
     # NOTE: Input node is the only node where the value
     # may be passed as an argument to forward().
     #
@@ -56,14 +57,105 @@ class Input(Node):
         if value is not None:
             self.value = value
 
+
 class Add(Node):
     def __init__(self, x, y):
         Node.__init__(self, [x, y])
 
     def forward(self):
         """
-        This
+        Perform addition on all inbound nodes.
         """
+        x_value = self.inbound_nodes[0].value
+        y_value = self.inbound_nodes[1].value
+        self.value = x_value + y_value
+
+        # Make Add work on any number of inputs
+
+
+class Linear(Node):
+    def __init__(self, inputs, weights, bias):
+        Node.__init__(self, [inputs, weights, bias])
+
+        # NOTE: The weights and bias properties here are not
+        # numbers, but rather references to other nodes.
+        # The weight and bias values are stored within the
+        # respective nodes.
+
+    def forward(self):
+        """
+        Set self.value  to the value of the linear function output.
+        """
+        inputs = self.inbound_nodes[0].value
+        weights = self.inbound_nodes[1].value
+        bias = self.inbound_nodes[2].value
+        self.value = np.dot(inputs, weights) + bias
+
+
+class Sigmoid(Node):
+    """
+    Sigmoid function to make decision based
+    on weighted features from data.
+    """
+    def __init__(self, node):
+        Node.__init__(self, [node])
+
+    def _sigmoid(self, x):
+        """
+        This method is separate from `forward` because it
+        will be used later with `backward` as well.
+
+        `x`: A numpy array-like object.
+
+        Return the result of the sigmoid function.
+        """
+        return 1. / (1. + np.exp(-x))  # `.` ensures type is a float
+
+    def forward(self):
+        input_value = self.inbound_nodes[0].value
+        self.value = self._sigmoid(input_value)
+
+
+def topological_sort(feed_dict):
+    """
+    Sort generic nodes in topological order using Kahn's Algorithm.
+
+    `feed_dict`: A dictionary where the key is a `Input` node and the value is the respective value feed to that node.
+
+    Returns a list of sorted nodes.
+    """
+
+    input_nodes = [n for n in feed_dict.keys()]
+
+    G = {}
+    nodes = [n for n in input_nodes]
+    while len(nodes) > 0:
+        n = nodes.pop(0)
+        if n not in G:
+            G[n] = {'in': set(), 'out': set()}
+        for m in n.outbound_nodes:
+            if m not in G:
+                G[m] = {'in': set(), 'out': set()}
+            G[n]['out'].add(m)
+            G[m]['in'].add(n)
+            nodes.append(m)
+
+    L = []
+    S = set(input_nodes)
+    while len(S) > 0:
+        n = S.pop()
+
+        if isinstance(n, Input):
+            n.value = feed_dict[n]
+
+        L.append(n)
+        for m in n.outbound_nodes:
+            G[n]['out'].remove(m)
+            G[m]['in'].remove(n)
+            # if no other incoming edges add to S
+            if len(G[m]['in']) == 0:
+                S.add(m)
+    return L
 
 
 def forward_pass(output_node, sorted_nodes):
@@ -74,7 +166,7 @@ def forward_pass(output_node, sorted_nodes):
 
         `output_node`: The output node of the graph (no outgoing edges).
         `sorted_nodes`: a topologically sorted list of nodes.
-    
+
     Returns the output node's value
     """
 
