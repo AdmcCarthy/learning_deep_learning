92275f2e3d0f98ae5b2c45de229b1af6978097b7
Adam James McCarthy <a.mccarthy.uk@gmail.com>
2017-08-05 22:04:42 +0200

    feat: Added Mean squared error
    This is changing forward pass which causes errors in earlier tests

--- MiniFlow/miniflow.py
+++ MiniFlow/miniflow.py
@@ -116,6 +116,35 @@ class Sigmoid(Node):
         self.value = self._sigmoid(input_value)
 
 
+class MSE(Node):
+    def __init__(self, y, a):
+        """
+        The mean squared error cost function.
+        Should be used as the last node for a network.
+        """
+        # Call the base class' constructor.
+        Node.__init__(self, [y, a])
+
+    def forward(self):
+        """
+        Calculates the mean squared error.
+        """
+        # NOTE: We reshape these to avoid possible matrix/vector broadcast
+        # errors.
+        #
+        # For example, if we subtract an array of shape (3,) from an array of shape
+        # (3,1) we get an array of shape(3,3) as the result when we want
+        # an array of shape (3,1) instead.
+        #
+        # Making both arrays (3,1) insures the result is (3,1) and does
+        # an elementwise subtraction as expected.
+        y = self.inbound_nodes[0].value.reshape(-1, 1)
+        a = self.inbound_nodes[1].value.reshape(-1, 1)
+
+        diff = y - a
+        self.value = np.mean(diff**2)
+
+
 def topological_sort(feed_dict):
     """
     Sort generic nodes in topological order using Kahn's Algorithm.
@@ -158,19 +187,14 @@ def topological_sort(feed_dict):
     return L
 
 
-def forward_pass(output_node, sorted_nodes):
+def forward_pass(graph):
     """
-    Performs a forward pass through a list of sorted nodes.
+    Performs a forward pass through a list of sorted Nodes.
 
     Arguments:
 
-        `output_node`: The output node of the graph (no outgoing edges).
-        `sorted_nodes`: a topologically sorted list of nodes.
-
-    Returns the output node's value
+        `graph`: The result of calling `topological_sort`.
     """
-
-    for n in sorted_nodes:
+    # Forward pass
+    for n in graph:
         n.forward()
-
-    return output_node.value
