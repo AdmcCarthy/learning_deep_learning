3005a367d061318309db2d2f64261c07202b9b97
Adam James McCarthy <a.mccarthy.uk@gmail.com>
2017-08-05 21:49:52 +0200

    feat: Add mean squared error test

--- MiniFlow/nn.py
+++ MiniFlow/nn.py
@@ -17,7 +17,8 @@ from miniflow import (
                       Add,
                       topological_sort,
                       forward_pass,
-                      Linear
+                      Linear,
+                      Sigmoid
                       )
 
 
@@ -68,7 +69,7 @@ def linear():
     graph = topological_sort(feed_dict)
     output = forward_pass(f, graph)
 
-    print(output) # should be 12.7 with this example
+    print(output)  # should be 12.7 with this example
 
 
 def linear_matrix():
@@ -93,5 +94,48 @@ def linear_matrix():
     print(output)
 
 
+def sigmoid_test():
+    X, W, b = Input(), Input(), Input()
+
+    f = Linear(X, W, b)
+    g = Sigmoid(f)
+
+    X_ = np.array([[-1., -2.], [-1, -2]])
+    W_ = np.array([[2., -3], [2., -3]])
+    b_ = np.array([-3., -5])
+
+    feed_dict = {X: X_, W: W_, b: b_}
+
+    graph = topological_sort(feed_dict)
+    output = forward_pass(g, graph)
+
+    """
+    Output should be:
+    [[  1.23394576e-04   9.82013790e-01]
+    [  1.23394576e-04   9.82013790e-01]]
+    """
+    print(output)
+
+
+def mse():
+    y, a = Input(), Input()
+    cost = MSE(y, a)
+
+    y_ = np.array([1, 2, 3])
+    a_ = np.array([4.5, 5, 10])
+
+    feed_dict = {y: y_, a: a_}
+    graph = topological_sort(feed_dict)
+    # forward pass
+    forward_pass(graph)
+
+    """
+    Expected output
+
+    23.4166666667
+    """
+    print(cost.value)
+
+
 if __name__ == "__main__":
-    linear()
+    sigmoid_test()
